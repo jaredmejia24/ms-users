2ccf102117dc26d5ddb390276c461f12309128e6
Jared Mejia <jaredmejia4440@gmail.com>
2023-01-07 13:52:29 -0600

    Find user by pk done

--- app/core/urls.py
+++ app/core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("login", auth_view.LoginView.as_view()),
     path("self", users_view.SelfUserView.as_view()),
     path("users", users_view.UserViewSet.as_view()),
+    path("users/<int:pk>", users_view.UserByPkView.as_view()),
     path("logout", auth_view.LogoutView.as_view()),
     path("register", auth_view.RegisterView.as_view())
 ]
